aa2d9468095d586b37b0dc60fe1c9071b4f863ca
Ryan <leachrp@comcast.net>
2018-09-02 18:29:22 -0700

    Added landing page, campgrounds page, and functionality to add new campground.

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-// "/" => "Hi there!"
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+    { name: "Salmon Creek", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f5c879a2e4bcbc_340.jpg" },
+    { name: "Granite Hill", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104496f5c879a2e4bcbc_340.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c879a2e4bcbc_340.jpg" }
+]
+
 app.get("/", function (req, res) {
-    res.send("Hi there!");
+    res.render("landing");
 });
 
-// "/bye" => "Goodbye!"
-app.get("/bye", function (req, res) {
-    res.send("Goodbye!");
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
-// "/dog" => "MEOW!"
-app.get("/dog", function (req, res) {
-    res.send("MEOW!");
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+app.get("*", function(req, res){
+    res.send("Sorry, page not found...What are you doing with your life?");
 });
 
 //Tell Express to listen for requests (start server)
 app.listen(3000, function () {
-    console.log("Server has started!!!");
+    console.log("Server started");
 });
