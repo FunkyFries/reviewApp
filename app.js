472cc359d4c242e11529e9cd14bd9fced95b98f6
Ryan <leachrp@comcast.net>
2018-09-16 08:48:08 -0700

    Added database and mongoose to app.js. Added styling to campgrounds, landing, and new pages

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express   = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/YelpCamp");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create({ name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg" }, function(err, campground){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Newly created campground");
+        console.log(campground);
+    }
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-    { name: "Salmon Creek", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f5c879a2e4bcbc_340.jpg" },
-    { name: "Granite Hill", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104496f5c879a2e4bcbc_340.jpg" },
-    { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f5c879a2e4bcbc_340.jpg" }
+    { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg" },
+    { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/12/08/17/45/lake-sara-1892494__340.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2018/05/16/15/49/camper-3406137__340.jpg" }
 ]
 
 app.get("/", function (req, res) {
