d4de40a506470413a0cb3a0a1f567570ebfe14a2
Ryan <leachrp@comcast.net>
2018-10-13 10:34:39 -0700

    Added styling to campground show page

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express   = require("express"),
 
 mongoose.connect("mongodb://localhost/YelpCamp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 seedDB();
 
